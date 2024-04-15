770829ccbd78a1668d1eb0377f86f6b7dd5b8f06
ColtonTshudy <coltont@vt.edu>
2024-04-15 15:10:41 -0400

    fixed app loop code location

--- Src/main.c
+++ Src/main.c
@@ -97,8 +97,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
     applicationLoop(&app);
+    /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
