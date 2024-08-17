40cea6afb226ffb7b0ed1e0dd726ba8812ec14ae
ColtonTshudy <coltont@vt.edu>
2024-08-17 19:06:24 -0400

    cmake cant find standard header files??

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -618,5 +618,6 @@ void assert_failed(uint8_t* file, uint32_t line)
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 	/* USER CODE END 6 */
+	printf("Wrong parameters value: file %s on line %d\r\n", file, line);
 }
 #endif /* USE_FULL_ASSERT */
